f56380f439ef29070af42a28f7281253aefafa73
Jorge Lopez <jlopezgarciagt@gmail.com>
2022-03-12 08:03:00 -0600

    Clase para adivinar, y nodoarbol

---
+++ AdivinarAnimalP3A/AdivinaAnimal.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdivinarAnimalP3A
+{
+    public class AdivinaAnimal
+    {
+        private static NodoArbol raiz;
+
+        public AdivinaAnimal()
+        {
+            raiz = new NodoArbol("Elefante");
+        }
+
+        public void Jugar()
+        {
+            NodoArbol nodo = raiz;
+
+            while (nodo != null)
+            {
+                if (nodo != null)
+                {
+                    if (nodo.izquierda != null)
+                    {
+                        //Hay una pregunta
+                        Console.WriteLine(nodo.valor);
+                        //nodo = (respuesta()) ? nodo.izquierda : nodo.derecha;
+                    }
+                    else
+                    {
+                        //Se tiene un animal
+                        Console.WriteLine($"Debe de ser un {nodo.valor}");
+                        if (respuesta())
+                        {
+                            Console.WriteLine("Gané!");
+                        }
+                        else
+                        {
+                            animalNuevo(nuevo);
+                        }
+                        nodo = null;
+                        return;
+                    }
+                }
+            }
+
+        } //fin del juego
+
+
+        public bool respuesta()
+        {
+            while (true)
+            {
+                string respuesta = Console.ReadLine().ToLower().Trim();
+
+                if (respuesta.Equals("si")) return true;
+                if (respuesta.Equals("no")) return false;
+                Console.WriteLine("La respuesta debe ser si o no");
+
+            }
+        }
+    }
+}
---
+++ AdivinarAnimalP3A/NodoArbol.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdivinarAnimalP3A
+{
+    /// <summary>
+    /// Creación de un nodo
+    /// </summary>
+    public class NodoArbol
+    {
+        public Object valor;
+        public NodoArbol izquierda;
+        public NodoArbol derecha;
+
+        /// <summary>
+        /// Constructor inicializa nodos hijos en null
+        /// </summary>
+        public NodoArbol()
+        {
+            valor = izquierda = derecha = null;
+        }
+
+        /// <summary>
+        /// Constructor que inicia el nodo con los valor dados como parametros
+        /// </summary>
+        /// <param name="valorNodo"></param>
+        public NodoArbol(Object valorNodo)
+        {
+            izquierda = derecha = null;
+            valor = valorNodo;
+        }
+
+        public NodoArbol(NodoArbol iz, Object valor, NodoArbol der)
+        {
+            this.valor = valor;
+            izquierda = iz;
+            derecha = der;
+        }
+    }
+}
